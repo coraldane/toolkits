ab983e9e9784b7ed60ccfe89454d371707507b96
coraldane <coraldane@163.com>
2022-07-10 18:27:35 +0800

    change set into generic type;

--- container/safe_set.go
+++ container/safe_set.go
@@ -2,64 +2,54 @@ package container
 
 import (
 	"sync"
+	"sync/atomic"
 )
 
-type SafeSet struct {
-	sync.RWMutex
-	M map[interface{}]bool
+type SafeSet[Value any] struct {
+	M sync.Map
 }
 
 func NewSafeSet() *SafeSet {
 	return &SafeSet{
-		M: make(map[interface{}]bool),
+		M: sync.Map{},
 	}
 }
 
-func (this *SafeSet) Add(val interface{}) {
-	this.Lock()
-	this.M[val] = true
-	this.Unlock()
+func (this *SafeSet[Value]) Add(val Value) {
+	this.M.Store(val, true)
 }
 
-func (this *SafeSet) Remove(val interface{}) {
-	this.Lock()
-	delete(this.M, val)
-	this.Unlock()
+func (this *SafeSet[Value]) Remove(val Value) {
+	this.M.Delete(val)
 }
 
-func (this *SafeSet) Clear() {
-	this.Lock()
-	this.M = make(map[interface{}]bool)
-	this.Unlock()
+func (this *SafeSet[Value]) Clear() {
+	this.M.Range(func(key, val any) bool {
+		this.M.Delete(key)
+		return true
+	})
 }
 
-func (this *SafeSet) Contains(val interface{}) bool {
-	this.RLock()
-	_, exists := this.M[val]
-	this.RUnlock()
-	return exists
+func (this *SafeSet[Value]) Contains(val Value) bool {
+	_, ok := this.M.Load(val)
+	return ok
 }
 
-func (this *SafeSet) Size() int {
-	this.RLock()
-	len := len(this.M)
-	this.RUnlock()
-	return len
+func (this *SafeSet[Value]) Size() int {
+	var rowCount int32
+	this.M.Range(func(key, val any) bool {
+		atomic.AddInt32(&rowCount, 1)
+		return true
+	})
+	return int(rowCount)
 }
 
-func (this *SafeSet) ToSlice() []interface{} {
-	this.RLock()
-	defer this.RUnlock()
-
-	count := len(this.M)
-	if count == 0 {
-		return []interface{}{}
-	}
-
-	r := []interface{}{}
-	for val := range this.M {
-		r = append(r, val)
-	}
-
-	return r
+func (this *SafeSet[Value]) ToSlice() []Value {
+	result := make([]Value, 0)
+	this.M.Range(func(key, val any) bool {
+		keyObj := key.(Value)
+		result = append(result, keyObj)
+		return true
+	})
+	return result
 }
